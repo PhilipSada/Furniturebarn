b4d25b9f3dc4d88990ac7b2fb23023ada46cadf8
Philip Sada <sadaphillip@gmail.com>
2020-05-16 23:35:56 +0200

    added meta tags

--- resources/views/layouts/home-page.blade.php
+++ resources/views/layouts/home-page.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" prefix="og: http://ogp.me/ns#">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,14 +11,12 @@
         <meta property="og:locale" content="en_US">
         <meta property="og:type" content="website">
         <meta property="og:title" content="Furniturebarn">
-        <meta property="og:image:url" content="http://furniturebarnng.com/images/preview.png">        
-        <meta property="og:image:secure_url" content="https://furniturebarnng.com/images/preview.png">        
+        <meta property="og:image" content="https://furniturebarnng.com/images/preview.png">            
         <meta property="og:image:type" content="image/png">        
         <meta property="og:image:width" content="1897">        
         <meta property="og:image:height" content="932">        
         <meta property="og:description" content="website for a furniture manufacturing company">
         <meta property="og:url" content="http://furniturebarnng.com/">
-        <meta property="og:secure_url" content="https://furniturebarnng.com/">
         <meta property="og:site_name" content="Furniturebarn">
         <meta name="twitter:card" content="summary_large_image">
         <meta name="twitter:title" content="Furniturebarn">
